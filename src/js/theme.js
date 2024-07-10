280538007bc782aab82d704756b83b7813068a9e
ringmaker <chrisringmaker@gmail.com>
2024-07-09 22:07:29 -0400

    working theme toggle

--- src/js/theme.js
+++ src/js/theme.js
@@ -7,6 +7,7 @@ const rightCircle = document.querySelector('.toggle-checked')
 // Save state of checkbox based on current theme chosen
 if (savedTheme) {
 	setTheme(savedTheme)
+	updateTogglePositions()
 } else {
 	// Set the state of checkbox to be neither true or false for first page load
 	themeToggle.indeterminate = true
@@ -14,19 +15,18 @@ if (savedTheme) {
 
 themeToggle.addEventListener('change', () => {
 	setTheme(themeToggle.checked ? 'Dark' : 'Light')
-	// themeToggle.checked
-	// 	? leftCircle.setAttribute('cx', 0)
-	// 	: leftCircle.setAttribute('cx', -10)
+	updateTogglePositions()
+})
 
+function updateTogglePositions() {
 	if (themeToggle.checked) {
 		leftCircle.setAttribute('cx', 8)
 		rightCircle.setAttribute('cx', 70)
 	} else {
 		leftCircle.setAttribute('cx', -22)
 		rightCircle.setAttribute('cx', 40)
-		// 48
 	}
-})
+}
 
 function setTheme(theme) {
 	themeToggle.checked = theme === 'Dark'
@@ -39,15 +39,18 @@ function setTheme(theme) {
 // This was the only way I could solve a bug that forced system darkmode users to click the checkbox twice.
 // The intention is for the users VERY FIRST load, set the theme and checkbox accordingly.
 if (!savedTheme) {
-	// Get the preceived lightness of an RGB value
-	const getLightness = (color) => {
-		const [r, g, b] = color.match(/\d+/g).map((n) => n / 255)
-		// Using WCAG 2 formula to get Relative luminance of how bright the current background is on users first page load
-		return 0.2126 * r + 0.7152 * g + 0.0722 * b
-	}
+	//
+	setTimeout(() => {
+		// Get the preceived lightness of an RGB value
+		const getLightness = (color) => {
+			const [r, g, b] = color.match(/\d+/g).map((n) => n / 255)
+			// Using WCAG 2 formula to get Relative luminance of how bright the current background is on users first page load
+			return 0.2126 * r + 0.7152 * g + 0.0722 * b
+		}
 
-	// Get the background color of the body and set
-	const bgColor = window.getComputedStyle(document.body).backgroundColor
-	const isDark = getLightness(bgColor) < 0.5
-	setTheme(isDark ? 'Dark' : 'Light')
+		// Get the background color of the body and set
+		const bgColor = window.getComputedStyle(document.body).backgroundColor
+		const isDark = getLightness(bgColor) < 0.5
+		setTheme(isDark ? 'Dark' : 'Light')
+	}, 5)
 }

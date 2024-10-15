2c68be9710ff3c2c317ef426d2cb3f91c1d35b05
ringmaker <chrisringmaker@gmail.com>
2024-10-15 09:58:44 -0400

    data received fix and state abbreviations added

--- src/js/index.js
+++ src/js/index.js
@@ -26,7 +26,7 @@ formSubmit.addEventListener('click', async (e) => {
 			`${formInput.value.toLowerCase()}`
 		)
 		// TODO fix this console log
-		console.log(`Weather data received: ${weatherData}`)
+		console.log('Weather data received:', weatherData)
 		updateUI(weatherData)
 	} catch (error) {
 		console.error(`Error fetching weather: ${error}`)
@@ -44,10 +44,69 @@ function updateUI(weatherData) {
 		todayCard.textContent = weatherData.message
 	} else {
 		todayCard.innerHTML = `
-      <h2>${weatherData.location.name}, ${weatherData.location.country}</h2>
+      <h2>${weatherData.location.name}, ${abbrState(
+			weatherData.location.region
+		)} ${weatherData.location.country}</h2>
       <p>Current: ${weatherData.current} ${weatherData.temp}°F</p>
       <p>Feels like: ${weatherData.feelsLike}°F</p>
       <img src="${weatherData.icon}" alt="${weatherData.current}">
     `
 	}
+}
+
+function abbrState(stateName) {
+	const stateList = {
+		Arizona: 'AZ',
+		Alabama: 'AL',
+		Alaska: 'AK',
+		Arkansas: 'AR',
+		California: 'CA',
+		Colorado: 'CO',
+		Connecticut: 'CT',
+		Delaware: 'DE',
+		Florida: 'FL',
+		Georgia: 'GA',
+		Hawaii: 'HI',
+		Idaho: 'ID',
+		Illinois: 'IL',
+		Indiana: 'IN',
+		Iowa: 'IA',
+		Kansas: 'KS',
+		Kentucky: 'KY',
+		Louisiana: 'LA',
+		Maine: 'ME',
+		Maryland: 'MD',
+		Massachusetts: 'MA',
+		Michigan: 'MI',
+		Minnesota: 'MN',
+		Mississippi: 'MS',
+		Missouri: 'MO',
+		Montana: 'MT',
+		Nebraska: 'NE',
+		Nevada: 'NV',
+		'New Hampshire': 'NH',
+		'New Jersey': 'NJ',
+		'New Mexico': 'NM',
+		'New York': 'NY',
+		'North Carolina': 'NC',
+		'North Dakota': 'ND',
+		Ohio: 'OH',
+		Oklahoma: 'OK',
+		Oregon: 'OR',
+		Pennsylvania: 'PA',
+		'Rhode Island': 'RI',
+		'South Carolina': 'SC',
+		'South Dakota': 'SD',
+		Tennessee: 'TN',
+		Texas: 'TX',
+		Utah: 'UT',
+		Vermont: 'VT',
+		Virginia: 'VA',
+		Washington: 'WA',
+		'West Virginia': 'WV',
+		Wisconsin: 'WI',
+		Wyoming: 'WY',
+	}
+
+	return stateList[stateName] || stateName
 }
